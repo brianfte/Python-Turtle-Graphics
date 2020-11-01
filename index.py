e10eeae1b802ff3b22e5e0fc70c21bdc017f51c3
brianfte <you@example.com>
2020-10-31 22:41:05 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,14 @@
+import turtle 
+
+window = turtle.Screen()
+brian = turtle.Turtle()
+brian.forward(50)
+brian.left(90)
+brian.forward(50)
+brian.left(90)
+brian.forward(50)
+brian.left(90)
+brian.forward(50)
+brian.left(90)
+
+turtle.mainloop()
